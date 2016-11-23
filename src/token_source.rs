11210c60a8c17440361564e7436a3141c1461c9f
Justin Carter <jcarter@starkandwayne.com>
2016-11-23 18:59:00 +0100

    Comment everything

--- src/token_source.rs
+++ src/token_source.rs
@@ -1,13 +1,13 @@
-package antlr
-
-type TokenSource interface {
-	NextToken() Token
-	Skip()
-	More()
-	GetLine() int
-	GetCharPositionInLine() int
-	GetInputStream() CharStream
-	GetSourceName() string
-	setTokenFactory(factory TokenFactory)
-	GetTokenFactory() TokenFactory
-}
+//package antlr
+//
+//type TokenSource interface {
+//	NextToken() Token
+//	Skip()
+//	More()
+//	GetLine() int
+//	GetCharPositionInLine() int
+//	GetInputStream() CharStream
+//	GetSourceName() string
+//	setTokenFactory(factory TokenFactory)
+//	GetTokenFactory() TokenFactory
+//}
